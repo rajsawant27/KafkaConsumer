cccdbd9183423d55cb6b4680811d379862a391d2
RAJ <rajsawant2722001@gmail.com>
2024-09-12 22:56:04 +0530

    Kafka consumer

---
+++ src/test/java/com/kafkaImpl/EndUser/EndUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaImpl.EndUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
